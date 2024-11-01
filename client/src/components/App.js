d20aa04b5eda312ffb56415ae4a29f76e1c6f934
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-11-01 18:46:33 -0300

    V 1.1.2

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from './Nav';
 
 function App() {
   return (
     <div>
+      <Nav />
       <h1>Logística Inteligente</h1>
     </div>
   );
---
+++ client/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Servicios</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
